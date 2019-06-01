35f06515c9e0aa4d4822282f6c05957443d042bb
icepluto <18792689010@163.com>
2019-06-01 23:57:32 +0800

    header

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="header">
+  <div class="header-left">返回</div>
+  <div class="header-input"> 输入城市/游玩/主题</div>
+  <div class="header-right">城市</div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .header
+    display flex
+    line-height 2.86rem
+    background-color #00bcd4
+    color #ffffff
+    .header-left
+      width 2.64rem
+      float left
+    .header-input
+      flex 1
+      background #fff
+      height 1.8rem
+      line-height 1.8rem
+      border-radius .5rem
+      margin-top 0.5rem
+      margin-left 0.5rem
+      color #cccccc
+    .header-right
+      width 4.24rem
+      float right
+      text-align center
+
+</style>
